6d5ed7ebcbee3f7a8472252204e2159a445398db
Nancy <butlern0513@gmail.com>
2019-03-06 14:26:31 -0700

    added shoe data

---
+++ dataEntries/shoeTypes.js
@@ -0,0 +1,35 @@
+
+const papa = require('papaparse')
+const fs = require('fs')
+
+
+const readCSV = ()=>{
+  let string = fs.readFileSync('./womens-shoes-prices/Datafiniti_Womens_Shoes.csv', 'utf-8')
+  let shoeJSONAll = papa.parse(string, {header:true})
+  return shoeJSONAll
+}
+
+let shoes = readCSV().data
+
+
+    let smallData = shoes.map(shoe=>{
+      return {
+        brand: shoe.brand,
+        colors: shoe.colors,
+        image_url: shoe.imageURLs,
+        name: shoe.name,
+        msrp: shoe[`prices.amountMax`]
+      }
+    })
+    smallData.forEach((shoe, i, arr)=>{
+      let name = shoe.name
+      let isDup = false
+      arr.forEach((otherShoe, index)=>{
+        if(otherShoe.name === name && index!==i){
+          isDup = true
+          arr.splice(index, 1)
+        }
+      })
+    })
+
+module.exports = smallData
